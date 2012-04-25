329bf93c1dfe8031cb4057561cb887285594ba6b
HouRay <hourui.me@gmail.com>
2012-04-25 22:20:08 +0800

    加入断点续传

--- img-php/job/job_download_pic.php
+++ img-php/job/job_download_pic.php
@@ -5,9 +5,11 @@ $ps = 5000;
 
 foreach(array('Region', 'POI') AS $collection) {
 	$count = $_SGLOBAL['db']->db->$collection->find()->count();
+	$func = $collection."_select";
 	for($i = 0; $i <= $count; $i ++) {
-		$res = $_SGLOBAL['db']->Region_select(null, array('name', 'center'), null, $ps, $i);
+		$res = $_SGLOBAL['db']->$func(null, array('name', 'center'), null, $ps, $i);
 		foreach($res AS $item) {
+			
 			$pic = $_SGLOBAL['flicker']->get_pic($item);
 			if(empty($pic)) {continue;}
 			$url = flicker_photo_url($pic[0], 'q');
@@ -15,9 +17,15 @@ foreach(array('Region', 'POI') AS $collection) {
 			!is_dir($dirname) && mkdir($dirname, 0755, true);
 			$filename = strval($item['_id']).'_1.png';
 			
-			$imagick = new Imagick();
-			$imagick->setformat('png');
-			
+			if(is_file($dirname.$filename)) {
+				continue;
+			}
+			try {
+				$imagick = new Imagick();
+				$imagick->setformat('png');
+			} catch (ImagickException $e) {
+				continue;
+			}
 			$fp = fopen("php://temp", "wrb");
 			$ch = curl_init();
 			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); 
@@ -32,8 +40,12 @@ foreach(array('Region', 'POI') AS $collection) {
 			if(curl_getinfo($ch, CURLINFO_HTTP_CODE) == '200') {
 				fseek($fp, 0);
 			} 
-			$imagick->readImageFile($fp);
-			$imagick->writeimage($dirname.$filename);
+			try {
+				$imagick->readImageFile($fp);
+				$imagick->writeimage($dirname.$filename);
+			} catch (ImagickException $e) {
+				continue;
+			}
 			echo "$collection {$item['name']}\n";
 		}
 	}
